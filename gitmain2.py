c38470ef5a6263bfbfdd0e2349d0099a274c9c7e
StormS0 <32385076+StormS0@users.noreply.github.com>
2022-09-14 11:35:50 +0800

    Update gitmain2.py

--- gitmain2.py
+++ gitmain2.py
@@ -13,5 +13,5 @@ for num in range(len(api_id)):
     client = TelegramClient('session_name', api_id[num], api_hash[num])
     client.start()
     for useid in sp_group_id:
-        real_id, peer_type = utils.resolve_id(useid)
+        real_id, peer_type = utils.resolve_id(int(useid))
         message = client.send_message(peer_type(real_id), '签到')    #第一项是机器人ID，第二项是发送的文字
