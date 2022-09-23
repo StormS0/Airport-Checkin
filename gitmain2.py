fb724e2261e7094f3238ff88533827b780b2ab9d
StormS0 <32385076+StormS0@users.noreply.github.com>
2022-09-23 14:14:47 +0800

    Update gitmain2.py

--- gitmain2.py
+++ gitmain2.py
@@ -10,11 +10,11 @@ api_hash = os.environ.get('api_hash')
 sp_group_id = group_id.split(",")
 client = TelegramClient('session_name', api_id[0], api_hash[0])
 client.start()
-for num in range(len(api_id)):
-    for useid in sp_group_id:
-        real_id, peer_type = utils.resolve_id(int(useid))
-        #message = client.send_message(peer_type(real_id), '签到')    #第一项是机器人ID，第二项是发送的文字
-        print("qiandao")
+
+for useid in sp_group_id:
+    real_id, peer_type = utils.resolve_id(int(useid))
+    #message = client.send_message(peer_type(real_id), '签到')    #第一项是机器人ID，第二项是发送的文字
+    print("qiandao")
 client.disconnect()
 
 
